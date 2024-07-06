8c836c7b625503308211c989aff6b195c91377ee
Mya Randolph <mya.randolph@gordon.edu>
2024-07-06 16:39:57 -0400

    staticfiles created

--- api/settings.py
+++ api/settings.py
@@ -125,7 +125,8 @@ USE_TZ = True
 # STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATIC_URL = '/static/'
-STATIC_ROOT = "pages/static/pages"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 MEDIA_URLS ='/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
